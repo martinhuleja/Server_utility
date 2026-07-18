f54a4ae6ce898d508b2448c8f5be639f79a2db10
Martin Huleja <xhulejm00@stud.fit.vut.cz>
2026-07-18 23:32:13 +0200

    feat: Parse arguments

--- upload_to_server.sh
+++ upload_to_server.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
+# --- GLOBAL DEFAULT VALUES ---
+DEFAULT_SERVER="merlin.fit.vutbr.cz"
+LOGIN="xhulejm00"
+
+DATE_TIME="$(date +%Y%m%d_%H%M)"
+DESTINATION="~/Documents/$DATE_TIME"
+
+SERVER="$DEFAULT_SERVER"
+FILE_TO_UPLOAD=""
+UPLOAD_DIR=false
+
 show_help() {
   printf -- "----- UPLOAD TO SERVER UTILITY -----\n"
   printf "> User Guide\n\n"
   printf "Options:\n"
   printf "  -h, --help                   Show this help message\n"
-  printf "  -s, --server <server>        Server to use (default: merlin.fit.vutbr.cz)\n"
+  printf "  -s, --server <server>        Server to use (default: %s)\n" "$DEFAULT_SERVER"
   printf "                               Use '-s eva' for eva.fit.vutbr.cz\n"
   printf "  -f, --file <file>            Upload specific file from current directory\n"
   printf "  -d, -r, --directory          Upload current directory\n"
@@ -17,8 +28,59 @@ show_help() {
   printf "Set default values in the begginning of the script!\n"
 }
 
+parse_arguments() {
+  if [ $# -eq 0 ]; then
+    printf "Error: No parameters provided.\n" >&2
+    show_help
+    exit 1
+  fi
+
+  while [[ $# -gt 0 ]]; do
+    case "$1" in
+    -h | --help)
+      show_help
+      exit 0
+      ;;
+    -s | --server)
+      if [[ "$2" == "eva" ]]; then
+        SERVER="eva.fit.vutbr.cz"
+      elif [[ -n "$2" && "$2" != -* ]]; then
+        SERVER="$2"
+      fi
+      shift 2
+      ;;
+    -f | --file)
+      if [[ -z "$2" || "$2" == -* ]]; then
+        printf "Error: Option -f, --file requires a file name.\n" >&2
+        exit 1
+      fi
+
+      FILE_TO_UPLOAD="$2"
+      shift 2
+      ;;
+    -d | -r | --directory)
+      UPLOAD_DIR=true
+      shift
+      ;;
+    -dst | --destination)
+      if [[ -z "$2" || "$2" == -* ]]; then
+        printf "Error: Option -dst, --destination requires a destination path.\n" >&2
+        exit 1
+      fi
+
+      DESTINATION="$2"
+      shift 2
+      ;;
+    *)
+      printf "Error: Unknown parameter %s. Use -h to show help message.\n" "$1" >&2
+      exit 1
+      ;;
+    esac
+  done
+}
+
 main() {
-  show_help
+  parse_arguments "$@"
 }
 
 main "$@"
