272aaf5a6912c1638fab7a4a52cfdb82ea37ee50
Martin Huleja <xhulejm00@stud.fit.vut.cz>
2026-07-18 17:40:07 +0200

    feat: Add README.md and help

---
+++ README.md
@@ -0,0 +1,21 @@
+# Upload to server utility
+
+Script `upload_to_server.sh` uploads a file or a folder to a server via `ssh`. Simplifies the process and saves time.
+
+## Setup
+
+Download `upload_to_server.sh` and use `chmod +x upload_to_server.sh`.
+
+## How to use it
+
+You can upload a file or whole folder.
+
+- `-h, --help`: Show help message
+- `-s, --server <server>`: Server to use
+  - *Note: Default server is `merlin.fit.vutbr.cz`. Use `-s eva` for `eva.fit.vutbr.cz`.*
+- `-f, --file <file>`: Upload specific file from current directory
+- `-d, -r, -directory`: Upload current folder
+- `-dst, --destination <path>`: Destionation folder on server
+  - *Default: `~/Documents/<YYYYMMDD_hhmm>`*
+
+**Set default values in the begginning of the script!**
---
+++ upload_to_server.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+show_help() {
+  printf -- "----- UPLOAD TO SERVER UTILITY -----\n"
+  printf "> User Guide\n\n"
+  printf "Options:\n"
+  printf "  -h, --help\n"
+  printf "  -s, --server <server>        Server to use (default: merlin.fit.vutbr.cz)\n"
+  printf "                               Use '-s eva' for eva.fit.vutbr.cz\n"
+  printf "  -f, --file <file>            Upload specific file from current directory\n"
+  printf "  -d, -r, --directory          Upload current directory\n"
+  printf "  -dst, --destination <path>   Destination folder on the server\n"
+  printf "                               (default: ~/Documents/YYYYMMDD_hhmm)\n\n"
+  printf "Examples:\n"
+  printf "  %s -f main.c\n" "$0"
+  printf "  %s --directory -s eva\n\n" "$0"
+  printf "Set default values in the begginning of the script!\n"
+}
+
+main() {
+  show_help
+}
+
+main "$@"
